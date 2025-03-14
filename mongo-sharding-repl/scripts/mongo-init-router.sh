2e5077aebcf03b4b2231c14add500f03c8879ccc
Bogdan <bogdan.homlet@gmail.com>
2025-03-14 11:13:05 +0400

    feat (spring2) | update configurations

--- mongo-sharding-repl/scripts/mongo-init-router.sh
+++ mongo-sharding-repl/scripts/mongo-init-router.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
-sh.addShard( "shard1/shard1_1:27030");
-sh.addShard( "shard2/shard2_1:27040");
+sh.addShard( "shard1/shard1_1:27030,shard1_2:27030,shard1_3:27030");
+sh.addShard( "shard2/shard2_1:27040,shard2_2:27040,shard2_3:27040");
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name": "hashed" });
--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-docker compose exec -T configSrv mongosh --port 27017 --quiet <<EOF
+docker compose exec -T configSrv mongosh --port 27019 --quiet <<EOF
 rs.initiate({
   _id: "config_server",
   configsvr: true,
   members: [
-    { _id: 0, host: "configSrv:27017" }
+    { _id: 0, host: "configSrv:27019" },
+    { _id: 1, host: "configSrv2:27019" },
+    { _id: 2, host: "configSrv3:27019" }
   ]
 })
 EOF
@@ -14,8 +16,8 @@ rs.initiate({
   _id: "shard1",
   members: [
     { _id: 0, host: "shard1_1:27030" },
-    { _id: 1, host: "shard1_2:27031" },
-    { _id: 2, host: "shard1_3:27032" }
+    { _id: 1, host: "shard1_2:27030" },
+    { _id: 2, host: "shard1_3:27030" }
   ]
 })
 EOF
@@ -25,8 +27,8 @@ rs.initiate({
   _id: "shard2",
   members: [
     { _id: 0, host: "shard2_1:27040" },
-    { _id: 1, host: "shard2_2:27041" },
-    { _id: 2, host: "shard2_3:27042" }
+    { _id: 1, host: "shard2_2:27040" },
+    { _id: 2, host: "shard2_3:27040" }
   ]
 })
 EOF
